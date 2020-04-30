1036d96b80b5f609b72e8fa9dca016badb4287c4
joker-x <ivan@sinanimodelucro.org>
2020-04-30 17:06:19 +0200

    Versión inicial

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+//
+// Módulos:
+//
+var gulp = require('gulp')
+  , del = require('del')
+  , fileInclude = require('gulp-file-include');
+
+//
+// Tareas de gulp
+//
+
+// limpiar
+gulp.task('limpiar', function() {
+  return del(['document_root/*']);
+});
+
+// copiamos assets a document_root
+gulp.task("copiar-assets", function() {
+   return gulp.src("src/assets/**")
+      .pipe(gulp.dest("document_root"));
+});
+
+// generar-html-estaticos
+gulp.task("generar-html-estaticos", function() {
+   return gulp.src(['src/plantillas/*.html', 'src/plantillas/*/*.html'])
+      .pipe(fileInclude({ 
+        prefix: '@@', 
+        basepath: 'src/bloques'
+      }))
+      .pipe(gulp.dest('document_root/'));
+});
+
+//
+// Tarea por defecto
+//
+gulp.task('default', gulp.series('limpiar', 'copiar-assets', 'generar-html-estaticos'));
